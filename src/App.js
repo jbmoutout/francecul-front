16b7c4ce388b6b21cb002750b2e857966eb780a2
Jean-Brice Moutout <jbmoutout@gmail.com>
2017-08-09 19:06:38 +0200

    minor UI Player fix

--- src/App.js
+++ src/App.js
@@ -1,61 +1,82 @@
-import React, { Component } from 'react';
-import Emissions from './Emissions'
-import './App.css';
-import parrot from './assets/images/parrot.gif';
+import React, { Component } from "react";
+import Emissions from "./Emissions";
+import "./App.css";
+import parrot from "./assets/images/parrot.gif";
 
 class App extends Component {
-
   state = {
-    emissions_count: {},
-  }
+    emissions_count: {}
+  };
 
   componentDidMount = () => {
     this.getEmissionsCount();
-  }
-
+  };
 
   getEmissionsCount = () => {
-    var root_url = 'https://hidden-basin-30200.herokuapp.com/emissions_count';
+    var root_url = "https://hidden-basin-30200.herokuapp.com/emissions_count";
     return fetch(root_url, {
-      method: 'GET',
-      headers: {
-      },
+      method: "GET",
+      headers: {}
     })
-      .then((response) => response.json())
-      .then((responseJson) => {
+      .then(response => response.json())
+      .then(responseJson => {
         console.log(responseJson);
-        this.setState({emissions_count: responseJson});
+        this.setState({ emissions_count: responseJson });
       })
-      .catch((error) => {
+      .catch(error => {
         console.error(error);
       });
-  }
+  };
 
-
+  showDetails = () => {
+    this.setState({ showDescription: true });
+  };
 
   render() {
     return (
       <div>
-        <div className="github-top">
-          <a href="https://github.com/jbmoutout/francecul"><h3>github</h3></a>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+            width: "75%",
+            margin: "auto"
+          }}
+        >
+          <h3 style={{ marginBottom: 0 }}>
+            francecul.<img src={parrot} width="10%" />
+          </h3>
+          <div className="github-top">
+            <a href="https://github.com/jbmoutout/francecul">
+              <h3>github</h3>
+            </a>
+          </div>
         </div>
-        <div style={{display:"flex", justifyContent: "center",}}>
-          <div style={{width:"75vw"}}>
-            <h1 style={{margin:0}}>Les pieds sur terre</h1>
-            <div style={{display:"flex", "justifyContent": "space-between"}}>
-              <h3 style={{marginBottom:0}}>francecul.<img src={parrot} width="10%"/></h3>
+
+        <div style={{ display: "flex", justifyContent: "center" }}>
+          <div style={{ width: "75vw" }}>
+            <h1 style={{ margin: 0 }}>Les pieds sur terre</h1>
+            <p
+              className={this.state.showDescription ? "hidden" : ""}
+              style={{ fontSize: "0.6em", textDecoration: "underline" }}
+              onClick={this.showDetails}
+            >
+              info
+            </p>
+
+            <div className={this.state.showDescription ? "" : "hidden"}>
+              <p style={{ marginBottom: 0, fontSize: "0.8em" }}>
+                Tous les jours, une demi-heure de reportage sans commentaire.<br />Par
+                Sonia Kronlund
+              </p>
             </div>
-            <div>
-              <p style={{marginBottom:0}}>Tous les jours, une demi-heure de reportage sans commentaire.<br></br>Par Sonia Kronlund</p>
-            </div>
-            <p style={{fontWeight: 800, marginTop:0}}>
+            <p style={{ fontWeight: 800, marginTop: 0 }}>
               {this.state.emissions_count + " emissions"}
             </p>
           </div>
         </div>
 
-        <Emissions  />
-
+        <Emissions />
       </div>
     );
   }
--- src/Player.js
+++ src/Player.js
@@ -1,9 +1,9 @@
-import React, { Component } from 'react';
-import ReactPlayer from 'react-player'
-import Duration from './Duration'
-import './Range.css';
-import play_btn from './assets/images/play.png'
-import pause_btn from './assets/images/pause.png'
+import React, { Component } from "react";
+import ReactPlayer from "react-player";
+import Duration from "./Duration";
+import "./Range.css";
+import play_btn from "./assets/images/play.png";
+import pause_btn from "./assets/images/pause.png";
 
 class Player extends Component {
   state = {
@@ -12,68 +12,81 @@ class Player extends Component {
     duration: 0,
     played: 0,
     playedSeconds: 0
-  }
+  };
 
   playPause = () => {
-    this.setState({ playing: !this.state.playing })
-  }
+    this.setState({ playing: !this.state.playing });
+  };
 
   setVolume = e => {
-   this.setState({ volume: parseFloat(e.target.value) })
- }
+    this.setState({ volume: parseFloat(e.target.value) });
+  };
 
-  onProgress = (state) => {
-    this.setState(state)
-  }
-
+  onProgress = state => {
+    this.setState(state);
+  };
 
   render() {
     const {
-      url, playing, volume,
-      played, loaded, duration,
+      url,
+      playing,
+      volume,
+      played,
+      loaded,
+      duration,
       playbackRate,
       soundcloudConfig,
       vimeoConfig,
       youtubeConfig,
       fileConfig
-    } = this.state
-    const SEPARATOR = ' · '
+    } = this.state;
+    const SEPARATOR = " · ";
     return (
       <div className="party-player">
         <button className="party-btn" onClick={this.playPause}>
-          {playing ?
-            <img src={pause_btn} width='50%' style={{paddingTop: "5px"}}
-            />:
-            <img src={play_btn} width='50%' style={{paddingTop: "5px",paddingLeft: "6px",}}
-            />}
+          {playing
+            ? <img src={pause_btn} width="50%" style={{ paddingTop: "5px" }} />
+            : <img
+                src={play_btn}
+                width="50%"
+                style={{ paddingTop: "5px", paddingLeft: "6px" }}
+              />}
         </button>
 
-        <section className='section'>
-          <div className='player-wrapper'>
-
+        <section className="section">
+          <div className="player-wrapper">
             <ReactPlayer
-              ref={player => { this.player = player }}
+              ref={player => {
+                this.player = player;
+              }}
               url={this.props.url}
               playing={playing}
-              className='react-player'
+              className="react-player"
               onProgress={this.onProgress}
               volume={volume}
               onReady={() => this.setState({ playing: true })}
-
               onDuration={duration => this.setState({ duration })}
-
             />
-            <div style={{marginLeft: "10%"}}>
-              <div style={{height: "40px"}}><h4>{this.props.title || "FranceCul"}</h4></div>
-              <div style={{position: "relative"}}><progress max={1} value={this.state.played} />
-                <div style={{position: "absolute", right:0  }}><Duration seconds={duration} /></div>
-                <div style={{position: "absolute", left:0  }}><Duration seconds={this.state.playedSeconds} /></div>
+            <div style={{ marginLeft: "10%" }}>
+              <div className="player-title-container">
+                <h4 className="player-title">
+                  {this.props.title || "FranceCul"}
+                </h4>
+              </div>
+              <div style={{ position: "relative" }}>
+                <progress max={1} value={this.state.played} />
+                <div style={{ position: "absolute", right: 0 }}>
+                  <Duration seconds={duration} />
+                </div>
+                <div style={{ position: "absolute", left: 0 }}>
+                  <Duration seconds={this.state.playedSeconds} />
+                </div>
               </div>
             </div>
           </div>
         </section>
       </div>
-        )
+    );
   }
 }
 
